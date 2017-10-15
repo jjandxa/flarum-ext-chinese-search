d0dfe82029c1191882078026425df3df2ce7af42
BuildTools <unconfigured@null.spigotmc.org>
2017-10-15 12:31:07 +0800

    modify version, add dist

---
+++ js/forum/dist/extension.js
@@ -0,0 +1,54 @@
+'use strict';
+
+System.register('jjandxa/hello/main', ['flarum/components/DiscussionList', 'flarum/components/DiscussionsSearchSource'], function (_export, _context) {
+    "use strict";
+
+    var DiscussionList, DiscussionsSearchSource;
+    return {
+        setters: [function (_flarumComponentsDiscussionList) {
+            DiscussionList = _flarumComponentsDiscussionList.default;
+        }, function (_flarumComponentsDiscussionsSearchSource) {
+            DiscussionsSearchSource = _flarumComponentsDiscussionsSearchSource.default;
+        }],
+        execute: function () {
+
+            app.initializers.add('hello', function () {
+
+                DiscussionList.prototype.loadResults = function (offset) {
+                    var preloadedDiscussions = app.preloadedDocument();
+
+                    if (preloadedDiscussions) {
+                        return m.deferred().resolve(preloadedDiscussions).promise;
+                    }
+
+                    var params = this.requestParams();
+                    params.page = { offset: offset };
+                    params.include = params.include.join(',');
+
+                    if (params.filter.q !== undefined && params.filter.q.indexOf('is:') === -1 && params.filter.q.indexOf('tag:') === -1) {
+                        return app.store.find('xun/discussions', params);
+                    }
+                    return app.store.find('discussions', params);
+                };
+
+                DiscussionsSearchSource.prototype.search = function (query) {
+                    var _this = this;
+
+                    query = query.toLowerCase();
+
+                    this.results[query] = [];
+
+                    var params = {
+                        filter: { q: query },
+                        page: { limit: 3 },
+                        include: 'relevantPosts,relevantPosts.discussion,relevantPosts.user'
+                    };
+
+                    return app.store.find('xun/discussions', params).then(function (results) {
+                        return _this.results[query] = results;
+                    });
+                };
+            });
+        }
+    };
+});
