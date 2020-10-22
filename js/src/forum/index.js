3ffcd229004aa3a9ac5846f08f111f77eb63f0d2
lostsnow <lostsnow@gmail.com>
2020-10-22 13:41:42 +0800

    fixes compatibility with beta14

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,45 +1,47 @@
 import app from 'flarum/app';
-import DiscussionList from 'flarum/components/DiscussionList';
+import {override} from 'flarum/extend';
+import DiscussionListState from 'flarum/states/DiscussionListState';
 import DiscussionsSearchSource from 'flarum/components/DiscussionsSearchSource';
 
-app.initializers.add('flarum-ext-chinese-search', function() {
+app.initializers.add('flarum-ext-chinese-search', function () {
+  override(DiscussionListState.prototype, 'loadResults', function (offset) {
+    const preloadedDiscussions = app.preloadedApiDocument();
 
-    DiscussionList.prototype.loadResults = function (offset) {
-        const preloadedDiscussions = app.preloadedApiDocument();
-        const params = this.requestParams();
-        params.page = {offset};
-        params.include = params.include.join(',');
-
-        if (preloadedDiscussions && params.filter.q !== undefined && params.filter.q.indexOf('is:') === -1
-            && params.filter.q.indexOf('tag:') === -1
-            && params.filter.q.indexOf('author:') === -1) {
-
-            const resultData = app.store.find('xun/discussions', params);
-
-            return m.deferred().resolve(resultData).promise;
-        } else if (preloadedDiscussions) {
-            return m.deferred().resolve(preloadedDiscussions).promise;
-        }
-
-        if (params.filter.q !== undefined && params.filter.q.indexOf('is:') === -1
-            && params.filter.q.indexOf('tag:') === -1
-            && params.filter.q.indexOf('author:') === -1) {
-
-            return app.store.find('xun/discussions', params);
-        }
-        return app.store.find('discussions', params);
+    if (preloadedDiscussions) {
+      return Promise.resolve(preloadedDiscussions);
     }
 
-    DiscussionsSearchSource.prototype.search = function (query) {
-        query = query.toLowerCase();
-        this.results[query] = [];
+    const params = this.requestParams();
+    params.page = {offset};
+    params.include = params.include.join(',');
 
-        const params = {
-            filter: {q: query},
-            page: {limit: 3},
-            include: 'mostRelevantPost'
-        };
-
-        return app.store.find('xun/discussions', params).then(results => this.results[query] = results);
+    if (preloadedDiscussions && params.filter.q !== undefined && params.filter.q.indexOf('is:') === -1
+      && params.filter.q.indexOf('tag:') === -1
+      && params.filter.q.indexOf('author:') === -1) {
+      const resultData = app.store.find('xun/discussions', params);
+      return Promise.resolve(resultData);
+    } else if (preloadedDiscussions) {
+      return Promise.resolve(preloadedDiscussions);
     }
+
+    if (params.filter.q !== undefined && params.filter.q.indexOf('is:') === -1
+      && params.filter.q.indexOf('tag:') === -1
+      && params.filter.q.indexOf('author:') === -1) {
+      return app.store.find('xun/discussions', params);
+    }
+
+    return app.store.find('discussions', params);
+  })
+
+  override(DiscussionsSearchSource.prototype, 'search', function (original, query) {
+    query = query.toLowerCase();
+    this.results[query] = [];
+    const params = {
+      filter: {q: query},
+      page: {limit: 3},
+      include: 'mostRelevantPost'
+    };
+
+    return app.store.find('xun/discussions', params).then((results) => (this.results[query] = results));
+  })
 });
